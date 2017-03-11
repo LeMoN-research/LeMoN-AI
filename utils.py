9228b99a7282a0e713deb18e1b4eafaf090fcabb
TheRussianFella <maximmanainen@gmail.com>
2017-03-11 18:22:20 +0000

    New

--- utils.py
+++ utils.py
@@ -5,10 +5,14 @@ import os
 import librosa
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+from librosa import load, logamplitude
+from librosa.feature import melspectrogram
+
 
 def positions_to_moves(positions):
     moves = np.array([np.subtract(positions[i+1, :, :3], positions[i, :, :3]) for i in range(len(positions)-1)])
     return moves
+
 
 def get_sound_from_vid(vid_path, save_dir="audio/"):
     name = vid_path.split("/")[-1].split(".")[0]
@@ -16,43 +20,54 @@ def get_sound_from_vid(vid_path, save_dir="audio/"):
     subprocess.call(command, shell=True)
     return save_dir+name+".wav"
 
+
 def get_sound_from_wav(wav_path, split_second=60, sr=44100):
     rec, sr = librosa.load(wav_path, sr=sr)
     pows_in_split = int(sr/split_second)
     pieces = np.array([rec[i:i+pows_in_split] for i in range(0, len(rec)-pows_in_split, pows_in_split)])
     return pieces
 
+
 def all_videos_to_sound(vid_dir="videos/"):
     for file in os.listdir(vid_dir):
         get_sound_from_vid(vid_dir+file, "audio/")
 
+        
 ### Dont use yet
 def get_data(npy_path):
     data_matr = np.load(npy_path)
     return np.array([np.array([y[0] for y in x]) for x in data_matr]),\
         np.array([np.array([y[1] for y in x]) for x in data_matr])
 
-def iterate_mini_batches(points, music, batch_size=100, block_size=15):
+    
+def get_spectrogram(y, sr):
+    S = melspectrogram(y, sr=sr, n_mels=200)
+    log_S = logamplitude(S, ref_power=np.max)
+    return log_S.T
+
+
+def iterate_minibatches(points, music, batch_size=100, block_size=15):
     blocks_left = [int(p.shape[0]/block_size) for p in points]
     moves = np.array([positions_to_moves(x) for x in points])
     batch_count = 0
-    batch = []
+    train_moves = []; pred_move = []; start_pos = []; out_music = []  
     while np.any(blocks_left != 0):
         inst = np.random.choice(range(len(blocks_left)))
         start = -blocks_left[inst]*block_size
-        fin = -blocks_left[inst]*block_size+block_size            
+        fin = start+block_size            
 
-        train_moves = moves[inst][start:fin-1]
-        pred_move = moves[inst][fin]
+        train_moves.append(moves[inst][start:fin-1])
+        pred_move.append(moves[inst][fin])
 
-        start_pos = points[inst][start]
+        start_pos.append(points[inst][start])
+        out_music.append(np.array([get_spectrogram(x, 43680).T for x in music[inst][start:fin]]))
 
-        batch.append(np.array([music[inst][start:fin], train_moves, start_pos, pred_move]))
         blocks_left[inst] -= 1
 
         batch_count += 1
 
         if batch_count == 100:
-            yield np.array(batch)
-            batch = []
+            yield np.array(music), np.array(train_moves), np.array(start_pos), np.array(pred_move)
+            train_moves = []; pred_move = []; start_pos = []; out_music = []  
             batch_count = 0
+            
